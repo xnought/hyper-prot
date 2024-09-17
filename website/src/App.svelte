1fb120b61cb66c7be1728326344d21ec0a94c988
xnought <dbertucci6@gatech.edu>
2024-09-17 15:39:15 -0400

    successfully load data

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import "./app.css";
 	import Header from "./components/Header.svelte";
 
 	import * as myc from "@apple/mycelium";
 	import "@apple/mycelium/dist/style.css";
+
+	onMount(async () => {
+		const d = await loadGraphData();
+		console.log(d);
+	});
+
+	interface GraphData {
+		nodes: string[];
+		edges: unknown[][];
+	}
+
+	async function loadGraphData() {
+		const d = await fetch("shared/test.json");
+		const j = await d.json();
+		return j as GraphData;
+	}
 </script>
 
 <Header />
